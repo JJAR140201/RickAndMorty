573a70f8d2d7447d37b47e2e4f18c3be0da736b9
cristiancaalvarez <cristiancaalvarez26@gmail.com>
2022-12-22 08:59:26 -0500

    Creacion componente Character

---
+++ src/components/Character.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Character = ({character}) => {
+  return (
+    <div className='row'>
+    <div className='col'> 
+    </div>
+    </div>
+  )
+}
+
+export default Character

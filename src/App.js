05e1a6b85a4a64b2d2e98801027c68bbdc08d1b8
JJAR140201 <avilesroajuanjose@gmail.com>
2022-12-22 10:25:35 -0500

    Boton de paginacion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Navbars from "./components/Navbars";
 import Characters from "./components/Characters";
+import Paginacion from "./components/Paginacion";
 
 function App() {
   const [characters, setCharacter] = useState([])
+  const [info, setInfo] = useState({})
   const initialUrl ="https://rickandmortyapi.com/api/character"
   const fetchCharecter = (initialUrl) => {
     fetch(initialUrl)
       .then(response => response.json())
-      .then(data => setCharacter(data.results))
+      .then((data) => {
+        setCharacter(data.results)
+        setInfo(data.info)
+      })
       .catch(error => console.log(error))
   }
 
@@ -16,11 +21,21 @@ function App() {
     fetchCharecter(initialUrl);
   }, [])
 
+  const onPrevious = () => {
+    fetchCharecter(info.prev)
+  }
+
+  const onNext = () => {
+    fetchCharecter(info.next)
+  }
+
   return (
     <>
     <Navbars brand="Rick and Morty" />
     <div className="container mt-5">
+      <Paginacion prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
       <Characters characters={characters}/>
+      <Paginacion/>
     </div>
     </>
   );
